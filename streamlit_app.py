2bed2cecca2e5104324286a15c66bf59a3691959
tmp25 <142102267+tmp25@users.noreply.github.com>
2023-08-12 22:57:11 -0600

    Update streamlit_app.py
    call Pandas library and read my_fruit_list from a CSV file

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,8 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
