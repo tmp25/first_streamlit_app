ee0968f0318af13c44a345bcb83f795f1733cc83
tmp25 <142102267+tmp25@users.noreply.github.com>
2023-08-12 22:58:36 -0600

    Update streamlit_app.py
    just modify 2nd header

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,6 @@ streamlit.text('🐔 Hard-boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
